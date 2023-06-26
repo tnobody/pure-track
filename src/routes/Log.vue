564ca3d5bbba08112a79536ee71da136ca4cb88a
Tim Keiner <tnobody@users.noreply.github.com>
2023-06-26 23:22:21 +0200

    fixed strict build issues

--- src/routes/Log.vue
+++ src/routes/Log.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
 import { usePocketbase } from '../composables/pocketbase'
-import { useAsyncState, useIntersectionObserver } from '@vueuse/core';
-import { VNodeRef, ref } from 'vue';
+import { useAsyncState } from '@vueuse/core';
+import { ref } from 'vue';
 
 type ExerciseSet = {
     id: string,
@@ -37,8 +37,8 @@ const index = ref(0)
     <div class="flex flex-col h-full justify-center relative overflow-hidden">
         <ul class="overflow-auto snap-y snap-mandatory max-h-full px-4">
             <li class="h-[calc(1_/_8_*_100%)]"></li>
-            <template v-for="(entry, i) of state" :key="entry.id">
-                <li v-for="(set, j) of entry.expand.sets" :key="set.id"
+            <template v-for="(entry) of state" :key="entry.id">
+                <li v-for="(set) of entry.expand.sets" :key="set.id"
                     class="snap-center py-2 h-3/4 gap-4 transform transition-transform ease-in-out">
                     <div class="p-4 h-full rounded bg-slate-900 flex flex-col">
                         <div class="self-end text-slate-300">{{ set.set }} / {{ entry.expand.sets.length }}</div>
